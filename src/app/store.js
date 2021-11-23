9a413a48a81315ec0737e66cef0bf8b878cb6edb
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-11-23 08:11:30 -0500

    feat: connect reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useReducer } from 'react';
+import userReducer from '../features/user';
 
 export const store = configureStore({
   reducer: {
-    user: useReducer,
+    user: userReducer,
   },
 });
